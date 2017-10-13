8b824f13c0045b59697ca9a0eccdd45c7af9dc5a
razcrisan <rcrisan13@gmail.com>
2017-10-13 11:20:05 -0400

    added classes, fields and constructors

---
+++ Bank_Account_Project/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_Project
+{
+    class Client
+    {
+        //Fields
+        private string firstName;
+        private int lastFour;
+        private string lastName;
+        
+        //Properties
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            
+        }
+
+        public int LastFour
+        {
+            get { return this.lastFour; }
+        }
+
+        public string LastName
+        {
+            get { return this.firstName; }
+        }
+
+        public Client()
+        {
+
+        }
+
+        public Client (string firstName, string lastName, int lastFour)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.lastFour = lastFour;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
