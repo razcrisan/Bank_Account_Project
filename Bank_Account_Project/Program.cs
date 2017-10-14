e4c027370da52cf5e5794dcb90f3cb15cfb11c96
razcrisan <rcrisan13@gmail.com>
2017-10-14 14:05:33 -0400

    errors coming up when implementing while loop in program

--- Bank_Account_Project/Program.cs
+++ Bank_Account_Project/Program.cs
@@ -11,16 +11,22 @@ namespace Bank_Account_Project
         static void Main(string[] args)
         {
             
-            Client client1 = new Bank_Account_Project.Client("Razvan", "Crisan", 1234);
+                Client client1 = new Bank_Account_Project.Client("Razvan", "Crisan", 1234);
                 Savings savings = new Savings(500.00);
                 Checking checking = new Checking(350.00);
 
-            double minBalance = 200.00;
-
-
 
             
+
+            string newTransaction;
+
+            do
+            {
                 //When user launches Applicating they will see this menu.
+
+
+
+                double minBalance = 200.00;
                 Console.WriteLine("Please enter the number of your desired option.");
                 Console.WriteLine("1. View Client Information");
                 Console.WriteLine("2. View Account Balance");
@@ -32,18 +38,16 @@ namespace Bank_Account_Project
 
                 int menuOptions = int.Parse(Console.ReadLine());
 
-
-          
-
                 if (menuOptions == 1)
                 {
                     client1.displayInfo();
                 }
-                else if (menuOptions == 2)
+                if (menuOptions == 2)
                 {
 
-                    Console.WriteLine("a: Checkings Account");
-                    Console.WriteLine("b: Savings Account");
+                    Console.WriteLine("a. Checkings Account");
+                    Console.WriteLine("b. Savings Account");
+                    Console.WriteLine("c. Return to Main Menu");
                     char menuOptions2 = char.Parse(Console.ReadLine().ToLower());
                     if (menuOptions2 == 'a')
                     {
@@ -61,24 +65,25 @@ namespace Bank_Account_Project
                     Console.WriteLine("Which account would you like to deposit into?");
                     Console.WriteLine("a. Checkings");
                     Console.WriteLine("b. Savings");
+                    Console.WriteLine("c. Return to Main Menu");
                     char menuOptions2 = char.Parse(Console.ReadLine().ToLower());
                     if (menuOptions2 == 'a')
                     {
                         Console.WriteLine("Enter deposit amount:");
                         double depositAmount = double.Parse(Console.ReadLine());
                         checking.Deposit();
-                    Console.WriteLine("Your new balance is: $" + (checking.Deposit()+depositAmount));
-                
-                   
+                        Console.WriteLine("Your new balance is: $" + (checking.Deposit() + depositAmount));
+
+
 
                     }
                     else if (menuOptions2 == 'b')
                     {
-                    Console.WriteLine("Enter deposit amount:");
-                    savings.depositAmount = double.Parse(Console.ReadLine());
-                    double newBalance = savings.Deposit();
-                    Console.WriteLine("Your new balance is: $" + (savings.Deposit()));
-                   
+                        Console.WriteLine("Enter deposit amount:");
+                        savings.depositAmount = double.Parse(Console.ReadLine());
+                        double newBalance = savings.Deposit();
+                        Console.WriteLine("Your new balance is: $" + (savings.Deposit()));
+
                     }
                 }
                 if (menuOptions == 4)
@@ -86,46 +91,53 @@ namespace Bank_Account_Project
                     Console.WriteLine("Which account would you like to withdraw from?");
                     Console.WriteLine("a. Checkings");
                     Console.WriteLine("b. Savings");
+                    Console.WriteLine("c. Return to Main Menu");
                     char menuOptions2 = char.Parse(Console.ReadLine().ToLower());
-                if (menuOptions2 == 'a')
-                {
-                    Console.WriteLine("Enter withdraw amount:");
-                    checking.withdrawAmount = double.Parse(Console.ReadLine());
-                    double newBalance = checking.Withdraw();
-                    if (newBalance < 0)
+                    if (menuOptions2 == 'a')
                     {
-                        Console.WriteLine("You cannot overdraft from checking.");
-                        Console.WriteLine("Enter new withdraw amount");
+                        Console.WriteLine("Enter withdraw amount:");
                         checking.withdrawAmount = double.Parse(Console.ReadLine());
-                        double newBalance2 = checking.Withdraw();               
-                        Console.WriteLine("Balance after withdraw is: $" + (checking.Withdraw()));
+                        double newBalance = checking.Withdraw();
+                        if (newBalance < 0)
+                        {
+                            Console.WriteLine("You cannot overdraft from checking.");
+                            Console.WriteLine("Enter new withdraw amount");
+                            checking.withdrawAmount = double.Parse(Console.ReadLine());
+                            double newBalance2 = checking.Withdraw();
+                            Console.WriteLine("Balance after withdraw is: $" + (checking.Withdraw()));
+                        }
                     }
-                }
-                else if (menuOptions2 == 'b')
-                {
-                    Console.WriteLine("Enter withdraw amount:");
-                    savings.withdrawAmount = double.Parse(Console.ReadLine());
-                    double NewBalance = savings.Withdraw();
-                    Console.WriteLine("Balance after withdraw is: $" + (savings.Withdraw()));
-                    if (minBalance <= 200.00)
+                    else if (menuOptions2 == 'b')
                     {
-                        Console.WriteLine("You are attempting to withdraw more");
-                        Console.WriteLine("than your minimum balance allows. Please enter new amount");
+                        Console.WriteLine("Enter withdraw amount:");
                         savings.withdrawAmount = double.Parse(Console.ReadLine());
+                        double NewBalance = savings.Withdraw();
                         Console.WriteLine("Balance after withdraw is: $" + (savings.Withdraw()));
+                        if (minBalance <= 200.00)
+                        {
+                            Console.WriteLine("You are attempting to withdraw more");
+                            Console.WriteLine("than your minimum balance allows. Please enter new amount");
+                            savings.withdrawAmount = double.Parse(Console.ReadLine());
+                            Console.WriteLine("Balance after withdraw is: $" + (savings.Withdraw()));
+                        }
                     }
-                }
 
                 }
                 if (menuOptions == 5)
                 {
                     Console.WriteLine("Thanks for choosing our bank");
+                    newTransaction = Console.ReadLine().Trim().Trim();
                 }
+                while (newTransaction == "YES")
+                    if (newTransaction == "NO")
+                    {
+                        Console.WriteLine("Thank you for visiting our bank, have a nice day!");
+                    }
 
 
 
 
-           
+            
 
         }
     }
